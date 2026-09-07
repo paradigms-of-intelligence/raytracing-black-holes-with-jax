5788ed5f8ab05d10991c1a950775d4c8f9782378
Thomas Fischbacher <tfish@google.com>
2026-09-07 15:41:40 +0200

    Extending PDF building: Also build the PDF from .tex

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-echo "Rebuilding PDFs from Markdown..."
+echo "Rebuilding 'extra material' PDFs from Markdown..."
 pandoc -s -f markdown -t pdf coordinate_transformations/ct_howto.md \
        -o coordinate_transformations/ct_howto.pdf
+
+echo "Rebuilding main article from TeX..."
+(cd tex; pdflatex main.tex; bibtex main; pdflatex main.tex; rm -f main.{aux,bbl,blg,log,out})
